c266a3b2b6422de22b43df34531ab603bb5b5f7e
abvavgjeremy <hello@funinternetthings.com>
2023-06-18 18:22:16 -0700

    tighten up site, remove about us, and add social icons

--- b4g/home/views.py
+++ b4g/home/views.py
@@ -21,36 +21,36 @@ def record_params(setup_state):
 # Helper functions
 
 
-def add_list_member(email):
-    MAILLIST = home.config['MAILGUN_MAILLIST']
-    MAILGUN_API_KEY = home.config['MAILGUN_API_KEY']
-    # TODO Add try/except for request fails
-    logger.info(f"Adding {email} to mail list")
-    return requests.post(
-        f"https://api.mailgun.net/v3/lists/{MAILLIST}/members",
-        auth=('api', MAILGUN_API_KEY),
-        data={
-            'subscribed': True,
-            'address': email
-        }
-    )
-
-
-def send_welcome_message(email):
-    MAILGUN_API_KEY = home.config['MAILGUN_API_KEY']
-    return requests.post(
-        "https://api.mailgun.net/v3/email.beforegreatness.com/messages",
-        auth=(
-            "api",
-            MAILGUN_API_KEY
-        ),
-        data={
-            "from": "Jeremy Schroeder <jeremy@beforegreatness.com>",
-            "to": email,
-            "subject": "Welcome to the Before Greatness Family!",
-            "template": "thank-you-for-subscribing"
-        }
-    )
+# def add_list_member(email):
+#     MAILLIST = home.config['MAILGUN_MAILLIST']
+#     MAILGUN_API_KEY = home.config['MAILGUN_API_KEY']
+#     # TODO Add try/except for request fails
+#     logger.info(f"Adding {email} to mail list")
+#     return requests.post(
+#         f"https://api.mailgun.net/v3/lists/{MAILLIST}/members",
+#         auth=('api', MAILGUN_API_KEY),
+#         data={
+#             'subscribed': True,
+#             'address': email
+#         }
+#     )
+#
+#
+# def send_welcome_message(email):
+#     MAILGUN_API_KEY = home.config['MAILGUN_API_KEY']
+#     return requests.post(
+#         "https://api.mailgun.net/v3/email.beforegreatness.com/messages",
+#         auth=(
+#             "api",
+#             MAILGUN_API_KEY
+#         ),
+#         data={
+#             "from": "Jeremy Schroeder <jeremy@beforegreatness.com>",
+#             "to": email,
+#             "subject": "Welcome to the Before Greatness Family!",
+#             "template": "thank-you-for-subscribing"
+#         }
+#     )
 
 
 # Basic site routes
@@ -71,30 +71,30 @@ def index():
     )
 
 
-@home.route('/about', methods=['GET'])
-def about():
-    """ About Us page """
-    logo_url = url_for('static', filename='images/BG_MTN_LOGO_WHITE-256w.png')
-    brand_logo_url = url_for('static', filename='images/BG_MTN_LOGO_horizontal-white-words.png')
-    video_url = url_for(
-        'static',
-        filename='videos/pexels-eberhard-grossgasteiger-857251-540x360-25fps.mp4'
-    )
-    return render_template(
-        'about.html',
-        video_url=video_url,
-        logo_url=logo_url,
-        brand_logo_url=brand_logo_url
-    )
+# @home.route('/about', methods=['GET'])
+# def about():
+#     """ About Us page """
+#     logo_url = url_for('static', filename='images/BG_MTN_LOGO_WHITE-256w.png')
+#     brand_logo_url = url_for('static', filename='images/BG_MTN_LOGO_horizontal-white-words.png')
+#     video_url = url_for(
+#         'static',
+#         filename='videos/pexels-eberhard-grossgasteiger-857251-540x360-25fps.mp4'
+#     )
+#     return render_template(
+#         'about.html',
+#         video_url=video_url,
+#         logo_url=logo_url,
+#         brand_logo_url=brand_logo_url
+#     )
 
 
-@home.route('/maillist', methods=['POST'])
-def maillist_post():
-    """ Subscribe to mail list endpoint """
-    email = request.form.get('email')
-    added = add_list_member(email=email)
-    flash("Thank you for your interest, please check your email for updates.")
-    logger.info(added.text)
-    # Send thank you message
-    send_welcome_message(email=email)
-    return render_template('index.html')
+# @home.route('/maillist', methods=['POST'])
+# def maillist_post():
+#     """ Subscribe to mail list endpoint """
+#     email = request.form.get('email')
+#     added = add_list_member(email=email)
+#     flash("Thank you for your interest, please check your email for updates.")
+#     logger.info(added.text)
+#     # Send thank you message
+#     send_welcome_message(email=email)
+#     return render_template('index.html')
